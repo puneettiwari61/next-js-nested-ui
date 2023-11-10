45590f0c5bc72fd019a9a6f34de6e79d1a861b31
Puneet Tiwari <puneettiwari@Puneets-MacBook-Pro.local>
2023-11-10 11:11:22 +0530

    updated siubsection

--- src/components/Subsection/index.js
+++ src/components/Subsection/index.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { memo, useState } from "react";
+import React, { useState } from "react";
 
 const Subsection = ({ data }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -44,4 +44,4 @@ const Subsection = ({ data }) => {
   );
 };
 
-export default memo(Subsection);
+export default Subsection;
